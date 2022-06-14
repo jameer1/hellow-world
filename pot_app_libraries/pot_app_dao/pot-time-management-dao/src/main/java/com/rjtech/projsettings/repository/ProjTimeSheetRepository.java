367d11af86b69cf01f52385474d69fab42926786
USER35 <USER35@DESKTOP-2VT2IGS>
2022-06-14 12:07:25 +0530

    multiple-entity's present with the same name soo we had given fully qualified name in the repositories to solve the 17 server runtime errors.

--- pot_app_libraries/pot_app_dao/pot-time-management-dao/src/main/java/com/rjtech/projsettings/repository/ProjTimeSheetRepository.java
+++ pot_app_libraries/pot_app_dao/pot-time-management-dao/src/main/java/com/rjtech/projsettings/repository/ProjTimeSheetRepository.java
@@ -12,17 +12,17 @@ import com.rjtech.timemanagement.timesheet.model.TimeSheetAdditionalTimeEntity;
 
 public interface ProjTimeSheetRepository extends JpaRepository<TimeSheetAdditionalTimeEntity, Long> {
 
-    @Query("SELECT T FROM TimeSheetAdditionalTimeEntity T where T.projId=:projId and T.projCrewMstrEntity.id=:crewId "
+    @Query("SELECT T FROM com.rjtech.timemanagement.timesheet.model.TimeSheetAdditionalTimeEntity T where T.projId=:projId and T.projCrewMstrEntity.id=:crewId "
     	+ "AND T.grantHrs is not null AND T.updatedOn >= TO_CHAR(SYSDATE-2, 'DD-MM-YYYY')")
     List<TimeSheetAdditionalTimeEntity> findCutOffDaysForCrew(@Param("projId") Long projId,
     		@Param("crewId") Long crewId);
     
-    @Query("SELECT T FROM TimeSheetAdditionalTimeEntity T where T.projId=:projId and T.projCrewMstrEntity.id is null "
+    @Query("SELECT T FROM com.rjtech.timemanagement.timesheet.model.TimeSheetAdditionalTimeEntity T where T.projId=:projId and T.projCrewMstrEntity.id is null "
         	+ "AND T.empId=:empId AND T.grantHrs is not null AND T.updatedOn >= TO_CHAR(SYSDATE-2, 'DD-MM-YYYY')")
         List<TimeSheetAdditionalTimeEntity> findCutOffDaysForIndividual(@Param("projId") Long projId,
         		@Param("empId") Long empId);
     
-    @Query("SELECT T FROM TimeSheetAdditionalTimeEntity T where T.projId=:projId  "
+    @Query("SELECT T FROM com.rjtech.timemanagement.timesheet.model.TimeSheetAdditionalTimeEntity T where T.projId=:projId  "
         	+ "AND T.timeSheetId =:timeSheetId")
         List<TimeSheetAdditionalTimeEntity> findAdditionalTimeAvailable(@Param("projId") Long projId,
         		@Param("timeSheetId") Long timeSheetId);
